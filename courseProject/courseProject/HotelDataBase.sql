dd877ea39e7aac7e8f2ee677aafd78ba3afe2534
user <user>
2016-10-24 15:38:41 +0300

    DataBase integration with WPF TEST

---
+++ courseProject/courseProject/HotelDataBase.sql
@@ -0,0 +1,17 @@
+﻿
+
+
+
+--SELECT * FROM Client
+
+--ALTER TABLE Client
+--DROP COLUMN CheckoutDate
+
+INSERT INTO public.Client (id,PassportData,AmountOfMoney,RoomNumber,AccomodationDate,CheckoutDate)
+VALUES (1, 'BC325467', 125.5 , 235, '20 september' , '27 september');
+
+INSERT INTO public.Client (id,PassportData,AmountOfMoney,RoomNumber,AccomodationDate,CheckoutDate)
+VALUES (2, 'BC353456', 900 , 007, '10 september' , '17 september');
+
+INSERT INTO public.Client (id,PassportData,AmountOfMoney,RoomNumber,AccomodationDate,CheckoutDate)
+VALUES (3, 'BC634643', 700.25 , 1337, '18 september' , '25 september');
--- courseProject/courseProject/model/Hotel.cs
+++ courseProject/courseProject/model/Hotel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace courseProject
 {
@@ -18,5 +20,26 @@ namespace courseProject
 
         public virtual List<Client> ClientL{get; set;}
         public virtual List<HotelRoom> HotelRoomL{get;set;} 
+
+        //test
+
+        void DataBaseConnect()
+        {
+            SqlConnection con = new SqlConnection();
+            SqlDataAdapter ad = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand();
+            String str = "SELECT id , roomclass , amountofpeople, priceforroom FROM HotelRoom";
+            cmd.CommandText = str;
+            ad.SelectCommand = cmd;
+            con.ConnectionString = "Data Source=localhost; Initial Catalog=Northwind; Integrated Security=True";
+            cmd.Connection = con;
+            DataSet ds = new DataSet();
+            ad.Fill(ds);
+            
+           // ListViewHotel.DataContext = ds.Tables[0].DefaultView;
+            con.Close();
+        }
+
+
     }
 }
