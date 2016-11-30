3d7448ba0e08b0440f8d65748488f986d8b7857e
user <user>
2016-11-30 22:24:51 +0200

    Added WPF control

---
+++ courseProject/courseProject/CustomControls/CustomControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace courseProject.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for CustomControl.xaml
+    /// </summary>
+    public partial class CustomControl : UserControl
+    {
+        public CustomControl()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string connStr = "SERVER=localhost;DATABASE=test;UID=root;PASSWORD=root;";
+            MySqlConnection con = new MySqlConnection(connStr);
+            MySqlCommand cmd = new MySqlCommand("select * from test.people", con);
+            con.Open();
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+            con.Close();
+
+            dtGrid.DataContext = dt;
+
+        }
+    }
+}
---
+++ courseProject/courseProject/model/FamilyApartment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace courseProject
+{
+    class FamelyRoom : HotelRoom
+    {
+        public FamelyRoom() { }
+
+        public int NumberOfBeds { set; get; }
+        public double PriceForChildRoom { set; get; }
+        public bool HasChildRoom { set; get; }
+
+        public double getPriceForChildRoom()
+        {
+            if (HasChildRoom == true)
+            {
+                return PriceForChildRoom;
+            }
+            else return 0;
+        }
+        public override double showPriceForRoom()
+        {
+            return PriceForRoom;
+        }
+
+    }
+}
